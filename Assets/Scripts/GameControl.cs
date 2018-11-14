5c2a9f21a6bd5e80c8b8fa5bbfedce66713c00d4
parkermattson <parkermattson@gmail.com>
2018-11-13 18:13:45 -0600

    Added GameController script
    Created the game controller script and made basic save and load functions

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+	
+	//Game variables
+	public int money;
+	public string playerName;
+	
+	
+
+	// Use this for initialization
+	void Awake () {
+		DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void Save()
+	{
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat");
+		
+		GameData saveData = new GameData();
+		//Assign local data to savedata here
+		saveData.money = money;
+		saveData.playerName = playerName;
+		
+		bf.Serialize(file, saveData);
+		file.Close();
+		
+	}
+	
+	public void Load()
+	{
+		if (File.Exists(Application.persistentDataPath + "/playerInfo.dat"))
+		{
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+			GameData loadData = (GameData)bf.Deserialize(file);
+			file.Close();
+			
+			//Assign loaded data to local variables here
+			money = loadData.money;
+			playerName = loadData.playerName;
+		}
+	}
+	
+	[Serializable]
+	class GameData
+	{
+		public int money;
+		public string playerName;
+		}
+	}
