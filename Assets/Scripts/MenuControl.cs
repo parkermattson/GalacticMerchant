59e2fe210904f4ce9764100222343236152c0509
parkermattson <parkermattson@gmail.com>
2018-11-27 19:45:36 -0600

    Added menucontrol script and assigned to buttons
    Added the menucontrol script and menucrontroller object for handling screen switching. Added script to buttons to enable screen switching functionality

---
+++ Assets/Scripts/MenuControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuControl : MonoBehaviour {
+
+	//Menu Vars
+	public GameObject activeScreen;
+	
+	public void screenChange(GameObject newScreen)
+	{
+		activeScreen.SetActive(false);
+		activeScreen = newScreen;
+		activeScreen.SetActive(true);
+	}
+}
