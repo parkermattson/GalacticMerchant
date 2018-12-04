bdb7bac05451919d416d825b874aa18180c501b5
parkermattson <parkermattson@gmail.com>
2018-12-03 23:24:46 -0600

    Re-added menuscript, added galaxy map placeholder image, adjusted anchors on travel screen

---
+++ Assets/Scripts/MenuControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuControl : MonoBehaviour {
+
+	public GameObject activeScreen;
+	
+	public void screenChange(GameObject newScreen)
+	{
+		activeScreen.SetActive(false);
+		activeScreen = newScreen;
+		activeScreen.SetActive(true);
+	}
+}
