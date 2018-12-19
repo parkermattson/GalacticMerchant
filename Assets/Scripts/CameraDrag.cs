0b90de35a7d6ad5671d4f34a1c7353beaf872ff4
Sorby-zz <zanerama0571@gmail.com>
2018-12-19 02:10:37 -0600

    Added garbage camera movement script

---
+++ Assets/Scripts/CameraDrag.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDrag : MonoBehaviour {
+
+    //ripped from DrOmega, StackOverflow
+    //https://stackoverflow.com/questions/43702017/unity-script-to-drag-camera
+
+    public float Zdist;
+    private Vector3 MouseStart;
+    private Vector3 derp;
+    public float speed = .0001f;
+
+    void Start()
+    {
+        Zdist = transform.position.z;  // Distance camera is above map
+    }
+
+    void LateUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MouseStart = new Vector3(Input.mousePosition.x * speed, Input.mousePosition.y * speed, Zdist);
+            MouseStart = Camera.main.ScreenToWorldPoint(MouseStart);
+            MouseStart.z = Zdist;
+
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            var MouseMove = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Zdist);
+            MouseMove = Camera.main.ScreenToWorldPoint(MouseMove);
+            MouseMove = new Vector3(-MouseMove.x * speed, -MouseMove.y * speed, MouseMove.z);
+            MouseMove.z = Zdist;
+            transform.position = (MouseMove - MouseStart);
+
+        }
+    }
+}
