1bc5517fc6a7c1ce602b9ca42b2d16918b172e67
parkermattson <parkermattson@gmail.com>
2018-12-21 18:39:44 -0600

    Added CharCreate script

--- Assets/Scripts/CharCreate.cs
+++ Assets/Scripts/CharCreate.cs
@@ -1,16 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CharCreate : MonoBehaviour {
+	
+	public int[] stats = {1, 1, 1, 1};
+	public GameObject[] statText = new GameObject[4];
 
-	// Use this for initialization
-	void Start () {
-		
+	public void StatTextUp(int statNum)
+	{
+		if (stats[statNum] < 5)
+			stats[statNum]++;
+		statText[statNum].GetComponent<TextMeshProUGUI>().SetText("{0}", stats[statNum]);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	public void StatTextDown(int statNum)
+	{
+		if (stats[statNum] > 1)
+			stats[statNum]--;
+		statText[statNum].GetComponent<TextMeshProUGUI>().SetText("{0}", stats[statNum]);
 	}
+	
 }
