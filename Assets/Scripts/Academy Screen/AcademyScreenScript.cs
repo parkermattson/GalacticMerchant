be35b52ae4fcc81a209a60741f2ecbb53dcd4e2a
parkermattson <parkermattson@gmail.com>
2019-04-09 22:08:14 -0500

    Changed format of academy screen ui slightly, made incomplete scripts for handling missions, moved scriptable objects into new folder

---
+++ Assets/Scripts/Academy Screen/AcademyScreenScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcademyScreenScript : MonoBehaviour {
+	
+	public GameObject missionBox;
+	public GameObject missionPrefab;
+	
+	public List<MissionTable> potentialMissions;
+	List<Mission> availableMissions;
+	
+
+	void UpdateMissionList()
+	{
+		GameObject tempBox;
+		
+		for (int i = 0; i < missionBox.transform.childCount; i++)
+		{
+			Destroy(missionBox.transform.GetChild(i).gameObject);
+		}
+		
+		for (int i = 0; i  < 5; i++)
+		{
+			tempBox = Instantiate(missionPrefab, missionBox.transform);
+			//Make mission box script and use here
+		}
+		
+	}
+}
---
+++ Assets/Scripts/Scriptable Object Scripts/MissionTable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New MissionTable", menuName = "MissionTable")]
+public class MissionTable : ScriptableObject {
+
+	public List<Mission> missionList;
+	public List<float> missionChance;
+	
+}
