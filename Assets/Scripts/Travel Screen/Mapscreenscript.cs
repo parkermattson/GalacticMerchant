797d4a9c3182de91c932ced7a187aa42cf8f9266
Sorby-zz <zanerama0571@gmail.com>
2019-07-17 02:11:54 -0500

    Working Travel Screen
    Map now sets playerLocation
    Travel screen status bar implemented
    
    Player ship needs set earlier on. Null reference exception thrown on travel screen.

--- Assets/Scripts/Travel Screen/Mapscreenscript.cs
+++ Assets/Scripts/Travel Screen/Mapscreenscript.cs
@@ -14,6 +14,27 @@ public class Mapscreenscript : MonoBehaviour, IBeginDragHandler, IDragHandler, I
     public GameObject hoverTooltip;
     GameObject selectedLocation = null;
     bool inTransit = false;
+    GameControl gcScript;
+
+    [SerializeField]
+    private GameObject gameController;
+
+    [SerializeField]
+    private GameObject hullBar;
+
+    [SerializeField]
+    private GameObject fuelBar;
+
+    [SerializeField]
+    private TextMeshProUGUI cargoText;
+
+    [SerializeField]
+    private TextMeshProUGUI moneyText;
+
+    void Awake()
+    {
+        gcScript = gameController.GetComponent<GameControl>();
+    }
 
     public void SetInTransit(bool transiting)
     {
@@ -22,19 +43,25 @@ public class Mapscreenscript : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 
     private void MoveShip()
     {
+        locationTooltip.SetActive(false);
         mapShipIcon.transform.localPosition = Vector2.MoveTowards(mapShipIcon.transform.localPosition, selectedLocation.transform.localPosition, 0.5f);
         if (Mathf.Abs((mapShipIcon.transform.localPosition-selectedLocation.transform.localPosition).magnitude)<=0.1f)
         {
             inTransit = false;
+            gcScript.playerLocation = selectedLocation.GetComponentInParent<LocationScript>().location;
         }
     }
 
     public void SelectLocation(GameObject newSelected)
     {
-        selectedLocation = newSelected;
-        locationTooltip.transform.GetChild(1).GetComponentInChildren<TextMeshProUGUI>().SetText(selectedLocation.GetComponent<LocationScript>().location.GetName() + "\n" + selectedLocation.GetComponent<LocationScript>().location.GetDescription());
-		locationTooltip.SetActive(true);
-		locationTooltip.transform.localPosition = selectedLocation.transform.localPosition;
+        if (!inTransit && !(Mathf.Abs((mapShipIcon.transform.localPosition - newSelected.transform.localPosition).magnitude) <= 0.1f))
+        {
+            selectedLocation = newSelected;
+            locationTooltip.SetActive(true);
+            locationTooltip.transform.localPosition = selectedLocation.transform.localPosition;
+            locationTooltip.transform.GetChild(1).GetComponentInChildren<TextMeshProUGUI>().SetText(selectedLocation.GetComponent<LocationScript>().location.GetName() + "\n" + selectedLocation.GetComponent<LocationScript>().location.GetDescription());
+            locationTooltip.transform.localPosition = selectedLocation.transform.localPosition;
+        }
     }
 
     public void OnBeginDrag(PointerEventData eventData)
@@ -48,7 +75,6 @@ public class Mapscreenscript : MonoBehaviour, IBeginDragHandler, IDragHandler, I
         mapImage.transform.position += mouseChange;
         mouseStart = Input.mousePosition;
         CheckBoundary();
-
     }
 
     private void CheckBoundary()
@@ -86,5 +112,39 @@ public class Mapscreenscript : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 			hoverTooltip.SetActive(false);
 		}
         if (inTransit) MoveShip();
+    }
+
+    void OnEnable()
+    {
+        SetHullBar();
+        SetFuelBar();
+        SetCargoSpace();
+        SetMoney();
+    }
+    void SetHullBar()
+    {
+        float hullPercent = (float)gcScript.shipState.currentHull / (float)gcScript.shipState.playerShip.maxHull;
+        hullBar.transform.localScale = new Vector3(hullPercent, 1, 1);
+
+        if (hullPercent > .66)
+            hullBar.GetComponent<Image>().color = Color.green;
+        else if (hullPercent > .33)
+            hullBar.GetComponent<Image>().color = Color.yellow;
+        else
+            hullBar.GetComponent<Image>().color = Color.red;
+
+    }
+    void SetFuelBar()
+    {
+        float fuelPercent = (float)gcScript.shipState.currentFuel / (float)gcScript.shipState.playerShip.maxFuel;
+        fuelBar.transform.localScale = new Vector3(fuelPercent, 1, 1);
+    }
+    void SetCargoSpace()
+    {
+        cargoText.SetText("Cargo Space: {0} / {1}", gcScript.shipState.currentCargo, gcScript.shipState.playerShip.maxCargo);
+    }
+    void SetMoney()
+    {
+        moneyText.SetText("Spacebucks: {0}", gcScript.playerMoney);
     }
 }
