2ac033f073523ce85f105d501588e9818d6927a6
Sorby-zz <zanerama0571@gmail.com>
2019-08-14 01:16:03 -0500

    Added factory script and additional ui for industry screen

---
+++ Assets/Scripts/Scriptable Object Scripts/Factory.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Factory : ScriptableObject {
+
+    private string factoryName;
+    private string factoryDescription;
+    private List<Recipe> recipteList;
+    private List<Recipe> queueRecipe;
+    private List<int> queueAmt;
+    private int currentQueueTime;
+    private bool isOwned;
+    
+
+
+}
